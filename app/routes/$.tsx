42fd1c1cf6e90199e685d9a8782126af34f8b51b
Nivekithan S <nivekithan363@gmail.com>
2023-04-09 19:45:42 +0530

    Project 2

---
+++ app/routes/$.tsx
@@ -0,0 +1,22 @@
+import { LoaderArgs, json } from "@remix-run/cloudflare";
+import { V2_MetaFunction, useLoaderData } from "@remix-run/react";
+
+export const loader = ({ request }: LoaderArgs) => {
+  const orignalUrl = request.headers.get("x-original-url");
+  return json({ requestUrl: request.url, orignalUrl });
+};
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  const { requestUrl, orignalUrl } = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <h1>Remix Website: 2</h1>
+      <p>Request URL: {requestUrl}</p>
+      <p>Orignal URL: {orignalUrl}</p>
+    </div>
+  );
+}
